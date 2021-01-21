bcd776c7fe52f550acbd70a2fe31df45a3609591
jairito11 <agust_sani@hotmail.com>
2021-01-21 07:31:42 -0600

    nuevos miembros completos

--- build/generated-sources/ap-source-output/fiuaemex/entities/Materias_.java
+++ build/generated-sources/ap-source-output/fiuaemex/entities/Materias_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-01-21T06:12:57")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-01-21T07:29:50")
 @StaticMetamodel(Materias.class)
 public class Materias_ { 
 
--- src/java/fiuaemex/backing/AdministradorBacking.java
+++ src/java/fiuaemex/backing/AdministradorBacking.java
@@ -5,9 +5,13 @@
  */
 package fiuaemex.backing;
 
+import fiuaemex.entities.Administradores;
 import fiuaemex.entities.Alumnos;
+import fiuaemex.entities.Asesores;
 import fiuaemex.entities.Materias;
+import fiuaemex.facade.AdministradoresFacadeLocal;
 import fiuaemex.facade.AlumnosFacadeLocal;
+import fiuaemex.facade.AsesoresFacadeLocal;
 import fiuaemex.facade.MateriasFacadeLocal;
 import java.io.Serializable;
 import java.util.List;
@@ -30,10 +34,17 @@ public class AdministradorBacking implements Serializable{
     private MateriasFacadeLocal materiasFacade;
     @EJB
     private AlumnosFacadeLocal alumnosFacade;
+    @EJB
+    private AsesoresFacadeLocal asesoresFacade;
+    @EJB
+    private AdministradoresFacadeLocal administradorFacade;
             
     private List<Materias> listaMaterias;
     private Materias materia;
     private Alumnos alumno;
+    private Asesores asesor;
+    private Administradores administrador;
+    
     /**
      * Creates a new instance of AdministradorBacking
      */
@@ -48,7 +59,25 @@ public class AdministradorBacking implements Serializable{
     public void init(){
         materia = new Materias();
         alumno = new Alumnos();
+        asesor = new Asesores();
+        administrador = new Administradores();
         consultar();
+    }
+
+    public Asesores getAsesor() {
+        return asesor;
+    }
+
+    public void setAsesor(Asesores asesor) {
+        this.asesor = asesor;
+    }
+
+    public Administradores getAdministrador() {
+        return administrador;
+    }
+
+    public void setAdministrador(Administradores administrador) {
+        this.administrador = administrador;
     }
 
     public Materias getMateria() {
@@ -82,6 +111,30 @@ public class AdministradorBacking implements Serializable{
             this.alumno.setMateriaId(materia);
             this.alumnosFacade.create(alumno);
             FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO,"Alumno creado correctamente","Alumno creado correctamente");
+            FacesContext.getCurrentInstance().addMessage(null, msg);
+            consultar();
+            init();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    public void nuevoAsesor(){
+        try{
+            this.asesoresFacade.create(asesor);
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO,"Asesor creado correctamente","Asesor creado correctamente");
+            FacesContext.getCurrentInstance().addMessage(null, msg);
+            consultar();
+            init();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    public void nuevoAdministrador(){
+        try{
+            this.administradorFacade.create(administrador);
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO,"Administrador creado correctamente","Administrador creado correctamente");
             FacesContext.getCurrentInstance().addMessage(null, msg);
             consultar();
             init();
